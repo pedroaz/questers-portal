5bbc1852bc6405861e7c6a8c69d531b9383fd5a5
Pedro Azevedo <pedro.azvm@gmail.com>
2025-06-25 16:38:38 +0200

    adding initial portal code (#1)
    * adding trigger changes
    
    * adding main page
    
    * adding content pages
    
    * adding initial cypress stuff
    
    * installing pnpm correctly
    
    * remove version
    
    * install node again
    
    * fixing cypress bug
    
    * fix start command
    
    * setting up correct cypress cloud project
    
    * adding readme
    
    * adding svelte flow
    
    * adding gitleaks
    
    * adding fake release
    
    * fixing small stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,31 @@
 <script lang="ts">
 	import '../app.css';
 
+	import posthog from 'posthog-js';
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import AppSidebar from './app-sidebar.svelte';
+
+	onMount(() => {
+		if (browser) {
+			posthog.init('phc_KSsuPOYsQfYrMaMH0nXb1PSMkgYS0cTKU2hv7dKmGmn', {
+				api_host: 'https://eu.i.posthog.com',
+				defaults: '2025-05-24',
+				person_profiles: 'always'
+			});
+		}
+	});
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<Sidebar.Provider>
+	<AppSidebar />
+	<main class="">
+		<!-- <Sidebar.Trigger /> -->
+		<div class="w-full">
+			{@render children?.()}
+		</div>
+	</main>
+</Sidebar.Provider>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to Questers</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Card from '$lib/components/ui/card/index.js';
+</script>
+
+<div class="flex w-full flex-col gap-8 p-16">
+	<h1 class="text-5xl">Questers Portal</h1>
+	<p>On the side bar you can find all about my game.</p>
+	<p>From Development to how to support it</p>
+	<h1 class="text-3xl">Dashboard (fake data)</h1>
+	<div class="flex flex-wrap justify-center gap-2">
+		<Card.Root>
+			<Card.Content>
+				<p class="text-2xl">10 registered user</p>
+			</Card.Content>
+		</Card.Root>
+		<Card.Root>
+			<Card.Content>
+				<p class="text-2xl">600 games played</p>
+			</Card.Content>
+		</Card.Root>
+	</div>
+</div>
