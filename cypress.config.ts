8c918e8fe608b15f4e145742e0e621d59bd7c898
Pedro Azevedo <pedro.azvm@gmail.com>
2025-06-25 13:02:21 +0200

    adding initial cypress stuff

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+	projectId: '2vjpyg',
+	e2e: {
+		// setupNodeEvents(on, config) {
+		//   // implement node event listeners here
+		// },
+		baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:5174'
+	}
+});
